a97c9a6c59a537d81b5fc4fb02519a047fd38c57
Tamilarasan-1 <tamilarasansiva01@gmail.com>
2026-04-17 17:21:32 +0530

    Remove /

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -6,7 +6,7 @@ export const handle: Handle = async ({ event, resolve }) => {
         return new Response(null, {
             headers: {
                 'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-                'Access-Control-Allow-Origin': 'https://electrician-react.vercel.app/',
+                'Access-Control-Allow-Origin': 'https://electrician-react.vercel.app',
                 'Access-Control-Allow-Headers': '*',
             }
         });
@@ -15,7 +15,7 @@ export const handle: Handle = async ({ event, resolve }) => {
     const response = await resolve(event);
 
     // Apply CORS headers to all other responses
-    response.headers.append('Access-Control-Allow-Origin', 'https://electrician-react.vercel.app/');
+    response.headers.append('Access-Control-Allow-Origin', 'https://electrician-react.vercel.app');
     response.headers.append('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     response.headers.append('Access-Control-Allow-Headers', '*');
 
